4f4139db8e2b46eedca0990e1b795f4f92b1f9ba
Andrew Lechowicz <andrew@alecho.com>
2019-09-26 19:51:11 -0400

    Add Chex to deps

--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule LiveChess.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:phoenix_live_view, "~> 0.3.0"}
+      {:phoenix_live_view, "~> 0.3.0"},
+      {:chex, path: "../chex"}
     ]
   end
 
