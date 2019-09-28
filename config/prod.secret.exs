573d8c95797e5a2d9d614c28fb95a0812bf2ffeb
Andrew Lechowicz <andrew@alecho.com>
2019-09-28 14:18:34 -0400

    Remove raises

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -4,24 +4,14 @@
 # remember to add this file to your .gitignore.
 use Mix.Config
 
-database_url =
-  System.get_env("DATABASE_URL") ||
-    raise """
-    environment variable DATABASE_URL is missing.
-    For example: ecto://USER:PASS@HOST/DATABASE
-    """
+database_url = System.get_env("DATABASE_URL")
 
 config :live_chess, LiveChess.Repo,
   # ssl: true,
   url: database_url,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
-secret_key_base =
-  System.get_env("SECRET_KEY_BASE") ||
-    raise """
-    environment variable SECRET_KEY_BASE is missing.
-    You can generate one by calling: mix phx.gen.secret
-    """
+secret_key_base = System.get_env("SECRET_KEY_BASE")
 
 config :live_chess, LiveChessWeb.Endpoint,
   http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
